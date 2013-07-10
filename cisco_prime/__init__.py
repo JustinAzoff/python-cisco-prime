d14b2d66e861bfcde0b7a468392f7f3571b9c230
Justin Azoff <justin@bouncybouncy.net>
2013-07-10 09:17:50 -0400

    working NCS client

---
+++ cisco_prime/__init__.py
@@ -0,0 +1,43 @@
+import requests
+import pyquery
+
+class LoginFailure(Exception):
+    pass
+
+class Client:
+    def __init__(self, host, username, password):
+        self.base = "https://%s/webacs/" % host
+        self.username = username
+        self.password = password
+        self.s = requests.Session()
+        self.s.verify=False
+        self.login()
+
+    def login(self):
+        data = {
+            'action': 'login',
+            'hasCorrectFlashVersion': 'true',
+            'spring-security-redirect': '/pages/common/index.jsp?&flashVersion=9.0.124.0&hasCorrectFlashVersion=true',
+            'username': self.username,
+            'j_username': self.username,
+            'password': self.password,
+            'j_password': self.password,
+        }
+        r = self.s.post(self.base + "j_spring_security_check", data=data)
+        r.raise_for_status()
+        if 'loginAction.do' in r.text:
+            raise LoginFailure()
+        return True
+
+    def search_client(self, mac_or_ip_or_name):
+        params = {
+            'searchType': 'macOrIpOrName',
+            'searchText': mac_or_ip_or_name,
+            'lastDetected': 1440,
+            'json': 'true',
+            'isAscending': 'false',
+            'orderByColumn': 'lastSessionTime',
+            'sort':  'lastSessionTime',
+        }
+        r = self.s.get(self.base + "searchClientAction.do", params=params)
+        return r.json()['items']
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='cisco-prime',
+    version=version,
+    description="Basic Cisco prime web client",
+    keywords='Cisco prime',
+    author='Justin Azoff',
+    author_email='JAzoff@albany.edu',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    zip_safe=False,
+    install_requires=[
+        "requests",
+    ],
+)
